06352d8f66e52f3b7c530608d86a44093dcba57e
Zachriasz Jankowski <jankowski.zachariasz@gmail.com>
2020-01-12 21:51:23 +0000

    Image handling

--- app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
@@ -76,17 +76,17 @@ public final class R {
         public static final int bottom = 0x7f080043;
         public static final int end = 0x7f080062;
         public static final int gone = 0x7f080071;
-        public static final int invisible = 0x7f08007b;
-        public static final int left = 0x7f080080;
-        public static final int packed = 0x7f080097;
-        public static final int parent = 0x7f080099;
-        public static final int percent = 0x7f08009c;
-        public static final int right = 0x7f0800a1;
-        public static final int spread = 0x7f0800c4;
-        public static final int spread_inside = 0x7f0800c5;
-        public static final int start = 0x7f0800ca;
-        public static final int top = 0x7f0800e5;
-        public static final int wrap = 0x7f0800f5;
+        public static final int invisible = 0x7f08007c;
+        public static final int left = 0x7f080081;
+        public static final int packed = 0x7f080099;
+        public static final int parent = 0x7f08009b;
+        public static final int percent = 0x7f08009e;
+        public static final int right = 0x7f0800a3;
+        public static final int spread = 0x7f0800c6;
+        public static final int spread_inside = 0x7f0800c7;
+        public static final int start = 0x7f0800cc;
+        public static final int top = 0x7f0800e7;
+        public static final int wrap = 0x7f0800f7;
     }
     public static final class styleable {
         private styleable() {}
--- app/src/main/java/com/receiptify/MainActivity.java
+++ app/src/main/java/com/receiptify/MainActivity.java
@@ -1,17 +1,34 @@
 package com.receiptify;
 
-
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import androidx.annotation.NonNull;
 import com.getbase.floatingactionbutton.FloatingActionsMenu;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
+import androidx.core.content.FileProvider;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.io.File;
 import static android.graphics.Color.argb;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    public static final String FILE_NAME = "temp.jpg";
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int GALLERY_PERMISSIONS_REQUEST = 0;
+    private static final int GALLERY_IMAGE_REQUEST = 1;
+    public static final int CAMERA_PERMISSIONS_REQUEST = 2;
+    public static final int CAMERA_IMAGE_REQUEST = 3;
+
+    private TextView mImageDetails;
+    private ImageView mMainImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mImageDetails = findViewById(R.id.image_details);
+        mMainImage = findViewById(R.id.main_image);
         createFabMenu();
 
     }
@@ -32,19 +51,13 @@ public class MainActivity extends AppCompatActivity {
         com.getbase.floatingactionbutton.FloatingActionButton takePhoto = new com.getbase.floatingactionbutton.FloatingActionButton(getBaseContext());
         takePhoto.setColorNormal(argb(255,255,0,0));
         takePhoto.setTitle("take a photo");
-        takePhoto.setOnClickListener(v -> {
-            //startCamera(); <-TODO: create a method for taking photos
-            Log.i("application-status","Start Camera!");
-            menuMultipleActions.collapse();});
+        takePhoto.setOnClickListener(v -> {startCamera();menuMultipleActions.collapse();});
 
 
         com.getbase.floatingactionbutton.FloatingActionButton loadPhoto = new com.getbase.floatingactionbutton.FloatingActionButton(getBaseContext());
         loadPhoto.setColorNormal(argb(255,0,255,0));
         loadPhoto.setTitle("point app to an existing photo from the phone's storage");
-        loadPhoto.setOnClickListener(v -> {
-            //startGalleryChooser(); <-TODO: create a method for choosing from gallery
-            Log.i("application-status","Start Gallery Chooser!");
-            menuMultipleActions.collapse();});
+        loadPhoto.setOnClickListener(v -> {startGalleryChooser(); menuMultipleActions.collapse();});
 
 
 
@@ -53,6 +66,69 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void startGalleryChooser() {
+       if (PermissionUtils.requestPermission(this, GALLERY_PERMISSIONS_REQUEST, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+
+            startActivityForResult(Intent.createChooser(intent, "Select a photo"),
+                    GALLERY_IMAGE_REQUEST);
+        }
+    }
+
+    public void startCamera() {
+        if (PermissionUtils.requestPermission(
+                this, CAMERA_PERMISSIONS_REQUEST,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA)) {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            Uri photoUri = FileProvider.getUriForFile(this, getApplicationContext().getPackageName() + ".FileProvider", getCameraFile());
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            startActivityForResult(intent, CAMERA_IMAGE_REQUEST);
+        }
+    }
+
+    public File getCameraFile() {
+        File dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        return new File(dir.getPath(), FILE_NAME);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_IMAGE_REQUEST && resultCode == RESULT_OK && data != null) {
+            uploadImage(data.getData());
+        } else if (requestCode == CAMERA_IMAGE_REQUEST && resultCode == RESULT_OK) {
+            Uri photoUri = FileProvider.getUriForFile(this, getApplicationContext().getPackageName() + ".FileProvider", getCameraFile());
+            uploadImage(photoUri);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case CAMERA_PERMISSIONS_REQUEST:
+                if (PermissionUtils.permissionGranted(requestCode, CAMERA_PERMISSIONS_REQUEST, grantResults)) {
+                    startCamera();
+                }
+                break;
+            case GALLERY_PERMISSIONS_REQUEST:
+                if (PermissionUtils.permissionGranted(requestCode, GALLERY_PERMISSIONS_REQUEST, grantResults)) {
+                    startGalleryChooser();
+                }
+                break;
+        }
+    }
+
+    //TODO sending image to CloudVision
+    public void uploadImage(Uri uri){
+        Log.i("application-status","Success!");
+    }
 
 
 }
